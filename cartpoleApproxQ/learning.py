ba33f6b29f97deb5309a441bb768e62e86621d0e
NickPham01 <npham01@college.harvard.edu>
2018-12-02 11:14:27 -0500

    Hill Climbing Fixed, see graphs

--- cartpoleApproxQ/learning.py
+++ cartpoleApproxQ/learning.py
@@ -7,19 +7,26 @@ import sys
 import matplotlib.pyplot as plt
 import copy
 
+"""================================================================================
+Hill Climbing Agent
+   ================================================================================"""
+
 class hillClimbAgent:
 
 	def __init__(self, envName):
 		self.env = gym.make(envName)
 		self.legalActions = [0,1]
-		self.env.observation_sgipace.n = len(self.env.observation_space.low)
+		self.env.observation_space.n = len(self.env.observation_space.low)
 
 		self.weights = np.random.rand(self.env.observation_space.n + 1) * 2 - 1
+		self.gamma = 0.9
 
+		self.gammas = []
 		self.episodes = []
 		self.rewards = []
 		self.epsilons = []
 		self.learnedWeights = []
+		self.bestRewards = []
 
 		print ("Init complete, weights initialized to\n", self.weights)
 
@@ -52,13 +59,12 @@ class hillClimbAgent:
 	def learn(self, episodes, render=False):
 		episodesPlot = range(episodes)
 
-		gamma = 0.1
 		prevWeights = self.weights.copy()
 		bestReward = float('-inf')
 
 		for episode in range(episodes):
 			# randomly explore nearby weights
-			self.weights = self.weights + (np.random.rand(self.env.observation_space.n + 1) * 2 - 1) * gamma
+			self.weights = self.weights + (np.random.rand(self.env.observation_space.n + 1) * 2 - 1) * self.gamma
 
 			reward = self.runEpisode(render)
 			if reward > bestReward:
@@ -69,6 +75,7 @@ class hillClimbAgent:
 			self.episodes.append(episode)
 			self.rewards.append(reward)
 			self.learnedWeights.append(self.weights.copy())
+			self.bestRewards.append(bestReward)
 
 			print("Episode", episode, "reward", reward)
 
@@ -78,22 +85,41 @@ class hillClimbAgent:
 			# 	# exit()
 			prevWeights = self.weights.copy()
 
+			self.gammas.append(self.gamma)
+			self.gamma *= 0.9
+
 
 		print("Learned Weights:", self.weights)
 
 	def plot(self):
-		plotDim = [1,1]
+		plotDim = [3,1]
 
 		def epPlot(yVals):
 			plt.plot(self.episodes, yVals)
 
+		plt.subplot(plotDim[0], plotDim[1],1)
 		epPlot(self.rewards)
+		plt.title("Rewards")
+
+		learnedWeights = np.array(self.learnedWeights.copy())
+		print (learnedWeights)
+
+		plt.subplot(plotDim[0], plotDim[1],2)
+		for i in range(learnedWeights.shape[1]):
+			epPlot(learnedWeights[:,i])
+		plt.title("Weights")
+
+		plt.subplot(plotDim[0], plotDim[1], 3)
+		epPlot(self.bestRewards)
+		plt.title("Running Best Reward")
 
 		plt.show(block=False)
 		input("Press Enter to Finish")
 		plt.close()
 
-
+"""================================================================================
+Approximate Q Learning Agent
+   ================================================================================"""
 
 class learningAgent:
 
